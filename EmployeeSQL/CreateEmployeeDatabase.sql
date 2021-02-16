98593449b1ae0579aec780cd3de49c0a0e4bf3c7
bentleyj68 <bentleyj68@gmail.com>
2021-02-16 18:06:30 +0800

    Initial Sprint

---
+++ EmployeeSQL/CreateEmployeeDatabase.sql
@@ -0,0 +1,78 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! Foreign Keys have been created using ALTER Table after all tables have been created.
+--       This avoids the need to creat tables in order.
+
+CREATE TABLE departments (
+    dept_no varchar(10) NOT NULL,
+    dept-name varchar(25) NOT NULL,
+    CONSTRAINT pk_departments PRIMARY KEY (
+        dept_no
+     )
+);
+
+CREATE TABLE dept_emp (
+    emp_no varchar(10) NOT NULL,
+    dept_no varchar(10) NOT NULL,
+    CONSTRAINT pk_dept_emp PRIMARY KEY (
+        emp_no,dept_no
+     )
+);
+
+CREATE TABLE dept_manager (
+    dept_no varchar(10) NOT NULL,
+    emp_no varchar(10) NOT NULL,
+    CONSTRAINT pk_dept_manager PRIMARY KEY (
+        dept_no,emp_no
+     )
+);
+
+--    Note: Refer to my Data folder in the GitHub repository to see how I used Excel 
+--          to convert and import employee dates using the PostgreSQL "Date" Data Type
+
+CREATE TABLE employees (
+    emp_no varchar(10) NOT NULL,
+    emp_title varchar(10) NOT NULL,
+    birth_date date NOT NULL,
+    first_name varchar(30) NOT NULL,
+    last_name varchar(30) NOT NULL,
+    sex char NOT NULL CHECK (sex in ('M','F')),
+    hire_date date NOT NULL,
+    CONSTRAINT pk_employees PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE salaries (
+    emp_no varchar(10) NOT NULL,
+    salary int NOT NULL,
+    CONSTRAINT pk_salaries PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE titles (
+    title_id varchar(10) NOT NULL,
+    title varchar(30) NOT NULL,
+    CONSTRAINT pk_titles PRIMARY KEY (
+        title_id
+     )
+);
+
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
+
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
+
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE employees ADD CONSTRAINT fk_employees_emp_title FOREIGN KEY(emp_title)
+REFERENCES titles (title_id);
+
+ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
---
+++ EmployeeSQL/queries.sql
@@ -0,0 +1,69 @@
+-- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+
+SELECT e.emp_no "Employee Number", e.last_name "Last Name", e.first_name "First Name", 
+	e.sex "Sex", CAST(s.salary AS MONEY) "Salary"
+FROM employees e
+JOIN salaries s ON e.emp_no = s.emp_no
+ORDER BY e.emp_no;
+
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
+--    Note: Refer to my Data folder in the GitHub repository to see how I used Excel 
+--          to convert and import employee dates using the PostgreQL Date Data Type
+--          Dates stored as 'YYYY-MM-DD'
+
+SELECT first_name "First Name", last_name "Last Name" , hire_date "Hire Date"
+FROM employees 
+WHERE EXTRACT(year FROM hire_date) = 1986;
+
+-- 3. List the manager of each department with the following information: department number, 
+--    department name, the manager's employee number, last name, first name.
+
+SELECT m.dept_no "Department Number", 
+	(select dept_name from departments d where d.dept_no = m.dept_no) "Department Name", 
+	e.emp_no "Employee Number",	e.last_name "Last Name", e.first_name "First Name"
+FROM dept_manager m, employees e
+WHERE m.emp_no = e.emp_no;
+
+-- 4. List the department of each employee with the following information: employee number, 
+--	  last name, first name, and department name.
+
+SELECT e.emp_no "Employee Number",	e.last_name "Last Name", e.first_name "First Name", 
+	d.dept_name "Department Name"
+FROM employees e
+JOIN dept_emp de ON de.emp_no = e.emp_no
+JOIN departments d ON d.dept_no = de.dept_no;
+
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules" 
+--    and last names begin with "B."
+
+SELECT first_name "First Name", last_name "Last Name", sex "Gender"
+FROM employees 
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+
+-- 6. List all employees in the Sales department, including their employee number, 
+--    last name, first name, and department name.
+
+-- Starting from departments
+SELECT e.emp_no "Employee Number",	e.last_name "Last Name", e.first_name "First Name",
+	d.dept_name "Department Name"
+FROM departments d 
+JOIN dept_emp de ON de.dept_no = d.dept_no AND d.dept_name = 'Sales'
+JOIN employees e ON e.emp_no = de.emp_no;
+
+-- Query with the same results, just starting from employees
+SELECT e.emp_no "Employee Number",	e.last_name "Last Name", e.first_name "First Name",
+	d.dept_name "Department Name"
+FROM employees e 
+JOIN dept_emp de ON de.emp_no = e.emp_no  
+JOIN departments d ON d.dept_no = de.dept_no AND d.dept_name = 'Sales';
+ 
+-- 7. List all employees in the Sales and Development departments, including their employee number, 
+--    last name, first name, and department name.
+
+SELECT e.emp_no "Employee Number",	e.last_name "Last Name", e.first_name "First Name",
+	d.dept_name "Department Name"
+FROM departments d 
+JOIN dept_emp de ON de.dept_no = d.dept_no AND d.dept_name IN ('Sales'
+JOIN employees e ON e.emp_no = de.emp_no;
+	
